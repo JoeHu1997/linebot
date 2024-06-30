8c16f0b7bdd3261c1af96eacb2877fc6a066cb54
JoeHu1997 <62299081+JoeHu1997@users.noreply.github.com>
2024-06-30 15:33:31 +0800

    test

--- views/line_bot.py
+++ views/line_bot.py
@@ -7,9 +7,22 @@ line_bot_bp = Blueprint('line_bot_bp', __name__)
 
 @line_bot_bp.before_app_first_request
 def setup_line_bot():
-    global line_bot_api, handler
-    line_bot_api = LineBotApi(current_app.config['LINE_CHANNEL_ACCESS_TOKEN'])
-    handler = WebhookHandler(current_app.config['LINE_CHANNEL_SECRET'])
+    current_app.line_bot_api = LineBotApi(current_app.config['LINE_CHANNEL_ACCESS_TOKEN'])
+    current_app.handler = WebhookHandler(current_app.config['LINE_CHANNEL_SECRET'])
+
+    @current_app.handler.add(MessageEvent, message=TextMessage)
+    def handle_message(event):
+        incoming_message = event.message.text
+        current_app.logger.info(f"Received message: {incoming_message}")
+
+        # 簡單回應相同的訊息
+        response_message = TextSendMessage(text=incoming_message)
+        
+        current_app.logger.info(f"Response message: {response_message}")
+        current_app.line_bot_api.reply_message(
+            event.reply_token,
+            response_message
+        )
 
 @line_bot_bp.route("/callback", methods=['POST'])
 def callback():
@@ -18,23 +31,9 @@ def callback():
     current_app.logger.info(f"Request body: {body}")
 
     try:
-        handler.handle(body, signature)
+        current_app.handler.handle(body, signature)
     except InvalidSignatureError:
         current_app.logger.error("Invalid signature. Check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    incoming_message = event.message.text
-    current_app.logger.info(f"Received message: {incoming_message}")
-
-    # 簡單回應相同的訊息
-    response_message = TextSendMessage(text=incoming_message)
-    
-    current_app.logger.info(f"Response message: {response_message}")
-    line_bot_api.reply_message(
-        event.reply_token,
-        response_message
-    )
